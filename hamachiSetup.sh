3badf4a79ceb9a4ba0b94e2452c8f7fb9366ce97
rishabh <91521594+rishabruh@users.noreply.github.com>
2021-12-08 19:41:58 +0530

    literally forgor to actually install hamachi

--- hamachiSetup.sh
+++ hamachiSetup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 echo ===============================
+echo Performing mandatory system upgrade
+echo ===============================
+sudo pacman -Syu
+yay -Syu
+
+echo ===============================
 echo "Installing flatpak (select 'N' if already installed)"
 echo ===============================
-sudo pacman -Syu flatpak
+sudo pacman -S flatpak
 
 echo ===============================
 echo Installing hamachi and haguichi
 echo ===============================
+yay -S logmein-hamachi
 sudo flatpak install haguichi 
 
 echo =============================== 
@@ -21,6 +28,6 @@ echo Checking daemon status
 echo ===============================
 sudo systemctl status logmein-hamachi.service
 
-echo ###############################
+echo ===============================
 echo Installation complete. Run hamachi --help to get started
-echo ###############################
+echo ===============================
